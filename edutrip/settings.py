be323556e280ef5e82d48119c5949593cea4395f
ezart <ezart193@gmail.com>
2018-08-08 19:46:26 +0300

    set debug=True

--- edutrip/settings.py
+++ edutrip/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6416%f4+&^u_(@%xs17^8r)ymmat^(4rj*pc%9lwsa2_rgfm2u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com' ]
 
