473e70229aa1a3cb31130745e07876e4c58c55a8
ezart <ezart193@gmail.com>
2018-08-15 17:38:51 +0300

    just a bunch of css and stuff

--- frontend/src/components/Components.js
+++ frontend/src/components/Components.js
@@ -31,10 +31,11 @@ const Nav = props =>{
     return (
       <div>
           <nav className="navbar navbar-expand-lg bg-light">
+              <a class="navbar-brand" href="#">Education trips</a>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div >
+              <div className="collapse navbar-collapse" >
                   <ul className="navbar-nav">
                       <li className="nav-item"><a className="nav-link" href="/">Home</a> </li>
                       <li className="nav-item"><a className="nav-link" href="#">Application Status</a> </li>
