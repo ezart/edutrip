bb1acb75f1663e70c256ce6348023a510d4fbaca
ezart <ezart193@gmail.com>
2018-08-17 15:42:10 +0300

    Revamped navbar

--- frontend/src/components/Components.js
+++ frontend/src/components/Components.js
@@ -30,23 +30,46 @@ const host ="http://edutrip.herokuapp.com/";
 const Nav = props =>{
     return (
       <div className="row">
-          <nav className="navbar  navbar-default fixed-top ">
-              <div className="container">
-                   <a className="navbar-brand">Education trips</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                   <div id="navbarNav" className="collapse navbar-collapse">
-                    <ul className="navbar-nav">
-                   <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-                   <li className="nav-item"><a className="nav-link" href="#">Application Status</a></li>
-                   <li className="nav-item"><a className="nav-link" href="#">Help</a></li>
-                   <li className="nav-item"><a className="nav-link" href="#">the Company</a></li>
-               </ul>
-               </div>
+          <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
+              <span class="navbar-toggler-icon leftmenutrigger"></span>
+              <a class="navbar-brand" href="/">EDUCATION TRIPS</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav animate side-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Home
+                      <span class="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Application status</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Help</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">KenGen</a>
+                  </li>
+                </ul>
+                <ul class="navbar-nav ml-md-auto d-md-flex">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Home
+                      <span class="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Application status</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Help</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">KenGen</a>
+                  </li>
+                </ul>
               </div>
           </nav>
       </div>
