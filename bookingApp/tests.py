5ee2214680eab1676b62ed2f927a73f61a7f3bb7
ezart <ezart193@gmail.com>
2020-10-05 18:46:20 +0300

    added django tests

--- bookingApp/tests.py
+++ bookingApp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Institution
 
-# Create your tests here.
+
+class InstitutionTestCase(TestCase):
+    def setUp(self):
+        Institution.objects.create(name="Parkland Boys", po_box="113 Parklands rd",
+                                   city="Nairobi", number_of_visitors=23, email="teacher@example.com")
+
+    def test_institution_name(self):
+        """Output name of school"""
+        school = Institution.objects.get(name="Parkland Boys")
+        self.assertEqual(school.__str__(), 'Parkland Boys')
